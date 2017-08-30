208b02a5f44c28fb1e1f6228193c0ee55cce9865
RandomKori <randomkori40@gmail.com>
2017-08-30 14:16:19 +0600

    v0.2

--- ForexNN1/ForexNN1/ForexNN1.py
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -15,10 +15,19 @@ def nn(x):
         m=cntk.layers.Dense(100,activation=cntk.tanh)(m)
     m=cntk.layers.Dense(3,activation=cntk.softmax)(m)
     return m
+def train(net,stream):
+    label=cntk.input_variable(3)
+    loss=cntk.cross_entropy_with_softmax(net,label)
+    label_error=cntk.classification_error(net,label)
+    learning_rate=0.2
+    lr_schedule=cntk.learning_rate_schedule(learning_rate,cntk.UnitType.minibatch)
+    learner=cntk.sgd(net.parameters,lr_schedule)
+    trainer=cntk.Trainer(net,(loss,label_error),[learner])
+    return
 
 data=LoadData("train.txt")
 x=cntk.input_variable(45)
-output=cntk.input_variable(3)
 net=nn(x)
+train(net,data)
 g=input("Нажмите любую клавишу")
 
