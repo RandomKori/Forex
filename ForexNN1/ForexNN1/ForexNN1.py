e3445ddb2510ecc2a4a3f14f583519554992b527
RandomKori <randomkori40@gmail.com>
2017-08-30 10:47:08 +0600

    v0.1 ForexNN1

--- ForexNN1/ForexNN1/ForexNN1.py
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -3,10 +3,11 @@ import cntk
 
 def LoadData(fn):
     n=os.curdir+"\\Data\\"+fn
-    label_dim=[]
-    input_dim=[]
-    data=cntk.io.StreamDef(None,(45,3),mlf=["Input","Ladel"])
-    rez=cntk.io.HTKMLFDeserializer(n,data)
+    datainp=cntk.io.StreamDef("Input",45)
+    dataout=cntk.io.StreamDef("Label",3)
+    dataall=cntk.io.StreamDefs(label=dataout,futures=datainp)
+    rez=cntk.io.CBFDeserializer(n,dataall)
     return rez
 
 data=LoadData("train.txt")
+
