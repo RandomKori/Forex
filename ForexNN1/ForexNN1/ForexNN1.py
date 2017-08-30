4d59b70175aa9e05d20f4c9bd6c0910aab4a0ff5
RandomKori <randomkori40@gmail.com>
2017-08-30 17:39:52 +0600

    v0.5

--- ForexNN1/ForexNN1/ForexNN1.py
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -18,53 +18,37 @@ def nn(x):
     m=cntk.layers.Dense(3,activation=cntk.softmax)(m)
     return m
 
-def moving_average(a, w=5):
-    if len(a) < w:
-        return a[:]    # Need to send a copy of the array
-    return [val if idx < w else sum(a[(idx-w):idx])/w for idx, val in enumerate(a)]
-
-
-# Defines a utility that prints the training progress
-def print_training_progress(trainer, mb, frequency, verbose=1):
-    training_loss = "NA"
-    eval_error = "NA"
-
-    if mb%frequency == 0:
-        training_loss = trainer.previous_minibatch_loss_average
-        eval_error = trainer.previous_minibatch_evaluation_average
-        if verbose: 
-            print ("Minibatch: {0}, Loss: {1:.4f}, Error: {2:.2f}%".format(mb, training_loss, eval_error*100))
-        
-    return mb, training_loss, eval_error
-
-def train(net,streamf):
-    label=cntk.input_variable(3)
-    loss=cntk.cross_entropy_with_softmax(net,label)
-    label_error=cntk.classification_error(net,label)
+def train(streamf):
+    input_var = cntk.input_variable(45)
+    label_var=cntk.input_variable(3)
+    net=nn(input_var)
+    loss=cntk.cross_entropy_with_softmax(net,label_var)
+    label_error=cntk.classification_error(net,label_var)
     learning_rate=0.2
     lr_schedule=cntk.learning_rate_schedule(learning_rate,cntk.UnitType.minibatch)
     learner=cntk.sgd(net.parameters,lr_schedule)
-    trainer=cntk.Trainer(net,(loss,label_error),[learner])
+    trainer=cntk.Trainer(net,(loss,label_error),learner)
     input_map={
-        input: streamf.streams.features,
-        label: streamf.streams.labels
+        input_var : streamf.streams.features,
+        label_var : streamf.streams.labels
     }
     training_progress_output_freq = 500
-    minibatch_size = 64
+    minibatch_size = 1
     num_samples_per_sweep = 60000
     num_sweeps_to_train_with = 10
     num_minibatches_to_train = (num_samples_per_sweep * num_sweeps_to_train_with) / minibatch_size
     for i in range(0,int(num_minibatches_to_train)):
         dat1=streamf.next_minibatch(minibatch_size,input_map = input_map)
         trainer.train_minibatch(dat1)
-        batchsize, loss, error = print_training_progress(trainer, i, training_progress_output_freq, verbose=1)
-        if error<2.0:
+        training_loss = trainer.previous_minibatch_loss_average
+        eval_error = trainer.previous_minibatch_evaluation_average
+        print ("Minibatch: {0}, Loss: {1:.4f}, Error: {2:.2f}%".format(i, training_loss, eval_error*100))
+        if eval_error*100<2.0:
             break
     return trainer.model
 
 data=LoadData("train.txt",True)
-x=cntk.input_variable(45)
-net=nn(x)
-model=train(net,data)
+
+model=train(data)
 g=input("Нажмите любую клавишу")
 
