b53586efaefd032316056e67947d4f3f911b2bd7
RandomKori <randomkori40@gmail.com>
2017-08-30 14:47:22 +0600

    v0.2

--- ForexNN1/ForexNN1/ForexNN1.py
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -4,7 +4,7 @@ def LoadData(fn):
     n=".\\Data\\"+fn
     datainp=cntk.io.StreamDef("Input",45)
     dataout=cntk.io.StreamDef("Label",3)
-    dataall=cntk.io.StreamDefs(labels=dataout,features=datainp)
+    dataall=cntk.io.StreamDefs(Label=dataout,Input=datainp)
     st=cntk.io.CTFDeserializer(n,dataall)
     rez=cntk.io.MinibatchSource(st)
     return rez
@@ -15,7 +15,27 @@ def nn(x):
         m=cntk.layers.Dense(100,activation=cntk.tanh)(m)
     m=cntk.layers.Dense(3,activation=cntk.softmax)(m)
     return m
-def train(net,stream):
+
+def moving_average(a, w=5):
+    if len(a) < w:
+        return a[:]    # Need to send a copy of the array
+    return [val if idx < w else sum(a[(idx-w):idx])/w for idx, val in enumerate(a)]
+
+
+# Defines a utility that prints the training progress
+def print_training_progress(trainer, mb, frequency, verbose=1):
+    training_loss = "NA"
+    eval_error = "NA"
+
+    if mb%frequency == 0:
+        training_loss = trainer.previous_minibatch_loss_average
+        eval_error = trainer.previous_minibatch_evaluation_average
+        if verbose: 
+            print ("Minibatch: {0}, Loss: {1:.4f}, Error: {2:.2f}%".format(mb, training_loss, eval_error*100))
+        
+    return mb, training_loss, eval_error
+
+def train(net,streamf):
     label=cntk.input_variable(3)
     loss=cntk.cross_entropy_with_softmax(net,label)
     label_error=cntk.classification_error(net,label)
@@ -23,6 +43,20 @@ def train(net,stream):
     lr_schedule=cntk.learning_rate_schedule(learning_rate,cntk.UnitType.minibatch)
     learner=cntk.sgd(net.parameters,lr_schedule)
     trainer=cntk.Trainer(net,(loss,label_error),[learner])
+    input_map={
+    label: streamf.streams.Label,
+    input: streamf.streams.Input
+    }
+    minibatch_size = 64
+    num_samples_per_sweep = 60000
+    num_sweeps_to_train_with = 10
+    num_minibatches_to_train = (num_samples_per_sweep * num_sweeps_to_train_with) / minibatch_size
+    for i in range(0,int(num_minibatches_to_train)):
+        dat=streamf.next_minibatch(minibatch_size,input_map=input_map)
+        trainer.train_minibatch(dat)
+        batchsize, loss, error = print_training_progress(trainer, i, training_progress_output_freq, verbose=1)
+        if error<2.0:
+            break
     return
 
 data=LoadData("train.txt")
