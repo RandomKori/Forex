aaf042a12983e6287cfcd1452b09894a07b09c2d
RandomKori <randomkori40@gmail.com>
2017-08-30 11:54:54 +0600

    v0.1 ForexNN1

---
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -0,0 +1,13 @@
+import os
+import cntk
+
+def LoadData(fn):
+    n=os.curdir+"\\Data\\"+fn
+    datainp=cntk.io.StreamDef("Input",45)
+    dataout=cntk.io.StreamDef("Label",3)
+    dataall=cntk.io.StreamDefs(labels=dataout,features=datainp)
+    st=cntk.io.CTFDeserializer(n,dataall)
+    rez=cntk.io.MinibatchSource(st)
+    return rez
+
+data=LoadData("train.txt")
