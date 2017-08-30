d57c63eaefbc79b6baa242d83481f8ebc4364103
RandomKori <randomkori40@gmail.com>
2017-08-30 12:02:08 +0600

    V0.1

--- ForexNN1/ForexNN1/ForexNN1.py
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -1,8 +1,7 @@
-import os
 import cntk
 
 def LoadData(fn):
-    n=os.curdir+"\\Data\\"+fn
+    n=".\\Data\\"+fn
     datainp=cntk.io.StreamDef("Input",45)
     dataout=cntk.io.StreamDef("Label",3)
     dataall=cntk.io.StreamDefs(labels=dataout,features=datainp)
