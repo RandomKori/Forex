30fc831cfc6f7facf81e2907441658477d1329b7
RandomKori <randomkori40@gmail.com>
2017-08-30 22:16:40 +0600

    v0.5

--- ForexNN1/ForexNN1/ForexNN1.py
+++ ForexNN1/ForexNN1/ForexNN1.py
@@ -8,8 +8,8 @@ def LoadData(fn,is_training):
     dataout=cntk.io.StreamDef("labels",3)
     dataall=cntk.io.StreamDefs(features=datainp,labels=dataout)
     st=cntk.io.CTFDeserializer(n,dataall)
-    rez=cntk.io.MinibatchSource(st,randomize = is_training,max_sweeps = cntk.io.INFINITELY_REPEAT if is_training else 1)
-    return rez
+    mbs=cntk.io.MinibatchSource(st,randomize = is_training,max_sweeps = cntk.io.INFINITELY_REPEAT if is_training else 1)
+    return mbs
 
 def nn(x):
     m=cntk.layers.Dense(100,activation=cntk.tanh)(x)
@@ -19,8 +19,8 @@ def nn(x):
     return m
 
 def train(streamf):
-    input_var = cntk.input_variable(45)
-    label_var=cntk.input_variable(3)
+    input_var = cntk.input_variable(45,np.float32)
+    label_var=cntk.input_variable(3,np.float32)
     net=nn(input_var)
     loss=cntk.cross_entropy_with_softmax(net,label_var)
     label_error=cntk.classification_error(net,label_var)
@@ -29,11 +29,12 @@ def train(streamf):
     learner=cntk.sgd(net.parameters,lr_schedule)
     trainer=cntk.Trainer(net,(loss,label_error),learner)
     input_map={
-        input_var : streamf.streams.features,
-        label_var : streamf.streams.labels
+        label_var : streamf.streams.labels,
+        input_var : streamf.streams.features
+        
     }
-    minibatch_size = 1
-    num_samples_per_sweep = 10
+    minibatch_size = 5000
+    num_samples_per_sweep = 1000
     for i in range(0,num_samples_per_sweep):
         dat1=streamf.next_minibatch(minibatch_size,input_map = input_map)
         trainer.train_minibatch(dat1)
@@ -45,16 +46,19 @@ def train(streamf):
     return trainer
 
 def test(streamf,trainer):
-    input_var = cntk.input_variable(45)
-    label_var=cntk.input_variable(3)
+    input_var = cntk.input_variable(45,np.float32)
+    label_var=cntk.input_variable(3,np.float32)
     test_input_map={
-        input_var : streamf.streams.features,
-        label_var : streamf.streams.labels
+        label_var : streamf.streams.labels,
+        input_var : streamf.streams.features
     }
-    test_minibatch_size=1
-    dat=streamf.next_minibatch(test_minibatch_size,input_map = test_input_map)
-    eval_error=trainer.test_minibatch(dat)
-    print("Average test error: {0:.2f}%".format(eval_error*100))
+    test_minibatch_size=1000
+    test_result = 0.0
+    for i in range(10):
+        dat=streamf.next_minibatch(test_minibatch_size,input_map = test_input_map)
+        eval_error=trainer.test_minibatch(dat)
+        test_result = test_result + eval_error
+    print("Average test error: {0:.2f}%".format(eval_error*100)/10)
     return
 
 data=LoadData("train.txt",True)
