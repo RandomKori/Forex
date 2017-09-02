e654a79e4e3c47581328da2bf7c5008bab430efb
RandomKori <randomkori40@gmail.com>
2017-09-02 09:45:11 +0600

    v1.3

--- ForexEval/ForexEval/ForexEval.cpp
+++ ForexEval/ForexEval/ForexEval.cpp
@@ -12,7 +12,7 @@ FunctionPtr model;
 
 void __stdcall LoadModel(wchar_t* s)
 {
-	std::wstring config=L".\\Models\\";
+	std::wstring config=L"./Models/";
 	config.append(s);
 	model->Load(config);
 }
@@ -30,11 +30,14 @@ void __stdcall EvalModel(double* inp, double* out)
 	inps = Value::CreateSequence(sp,v,d);
 	NDShape sp1(3, 1);
 	outs = Value::CreateSequence(sp1, v1, d);
-	Variable var1(model);
+	Variable var1;
+	var1= InputVariable({ 45 }, AsDataType<double>(), L"features");
+	Variable var2;
+	var2 = InputVariable({ 3 }, AsDataType<double>(), L"labels");
 	std::unordered_map<Variable, ValuePtr> inputLayer;
 	std::unordered_map<Variable, ValuePtr> outputLayer;
 	inputLayer.insert(std::pair<Variable, ValuePtr>(var1, inps));
-	outputLayer.insert(std::pair<Variable, ValuePtr>(var1, outs));
+	outputLayer.insert(std::pair<Variable, ValuePtr>(var2, outs));
 	model->Evaluate(inputLayer, outputLayer);
 	for (int i = 0; i < 3; i++)
 		out[i] = v1[i];
