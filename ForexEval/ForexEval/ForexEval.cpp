8522b5f0450c84d43ff20243aca8e32e8065d945
RandomKori <randomkori40@gmail.com>
2017-09-02 06:12:27 +0600

    v1.2

--- ForexEval/ForexEval/ForexEval.cpp
+++ ForexEval/ForexEval/ForexEval.cpp
@@ -3,23 +3,28 @@
 
 #include "stdafx.h"
 
-extern "C"  __declspec(dllexport) void LoadModel(char* s);
-extern "C" __declspec(dllexport) void EvalMoswl(double inp[], double out[]);
+extern "C"  __declspec(dllexport) void __stdcall LoadModel(wchar_t* s);
+extern "C" __declspec(dllexport) void __stdcall EvalModel(double* inp, double* out);
 
 using namespace Microsoft::MSR::CNTK;
 
 
 IEvaluateModel<double>* model;
 
-void LoadModel(char* s)
+void __stdcall LoadModel(wchar_t* s)
 {
-	std::string config(".\\Models\\");
-	config.append(s);
+	std::string config("deviceId=auto minibatchSize=1024 modelPath=\"./Models/");
+	char s1[256];
+	size_t len=std::wcstombs(s1,s, wcslen(s));
+	if (len > 0u)
+		s1[len] = '\0';
+	config.append(s1);
+	config.append("\"");
 	GetEvalD(&model);
-	model->CreateNetwork(config);
+	model->Init(config);
 }
 
-void EvalMoswl(double inp[], double out[])
+void __stdcall EvalModel(double* inp, double* out)
 {
 	std::vector<double> v(45);
 	for (int i = 0; i < 45; i++)
@@ -28,7 +33,7 @@ void EvalMoswl(double inp[], double out[])
 	std::map<std::wstring, std::vector<double>*> inpt;
 	inpt.insert(std::map<std::wstring, std::vector<double>*>::value_type(s, &v));
 	std::vector<double> v1(3);
-	std::wstring s1 = L"labwls";
+	std::wstring s1 = L"labels";
 	std::map<std::wstring, std::vector<double>*> outt;
 	outt.insert(std::map<std::wstring, std::vector<double>*>::value_type(s1, &v1));
 	model->Evaluate(inpt, outt);
