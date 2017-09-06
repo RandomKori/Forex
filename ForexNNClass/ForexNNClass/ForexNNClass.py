212da434dc8fed05e20d65466168b0ff02fb9767
RandomKori <randomkori40@gmail.com>
2017-09-07 02:15:57 +0600

    v1.6b

--- ForexNNClass/ForexNNClass/ForexNNClass.py
+++ ForexNNClass/ForexNNClass/ForexNNClass.py
@@ -1,35 +1,36 @@
 from __future__ import print_function
 import numpy as np
 import cntk
+from cntk.ops.functions import load_model
 
 def LoadData(fn,is_training):
     n=".\\Data\\"+fn
     datainp=cntk.io.StreamDef("features",45)
-    dataout=cntk.io.StreamDef("labels",4,is_sparse=True)
+    dataout=cntk.io.StreamDef("labels",3)
     dataall=cntk.io.StreamDefs(features=datainp,labels=dataout)
     st=cntk.io.CTFDeserializer(n,dataall)
     mbs=cntk.io.MinibatchSource(st,randomize = is_training,max_sweeps = cntk.io.INFINITELY_REPEAT if is_training else 1)
     return mbs
 
 def nn(x):
-    m=cntk.layers.Recurrence(cntk.layers.LSTM(45,activation=cntk.tanh))(x)
+    m=cntk.layers.Stabilizer()(x)
     for i in range(0,20):
-         m=cntk.layers.Recurrence(cntk.layers.LSTM(200,activation=cntk.tanh))(m)
-    m=cntk.layers.Recurrence(cntk.layers.LSTM(4,activation=cntk.tanh))(m)
+         m=cntk.layers.Recurrence(cntk.layers.LSTM(45))(m)
+    m=cntk.layers.Recurrence(cntk.layers.LSTM(3))(m)
     return m
 
 input_var = cntk.input_variable(45,np.float32, name = 'features',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
-label_var=cntk.input_variable(4,np.float32, name = 'labels',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+label_var=cntk.input_variable(3,np.float32, name = 'labels',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
 
 def train(streamf):
     global net
     net=nn(input_var)
-    loss = cntk.cross_entropy_with_softmax(net,label_var)
-    error=cntk.classification_error(net,label_var)
+    loss = cntk.squared_error(net,label_var)
+    error=cntk.squared_error(net,label_var)
     learning_rate=0.001
     lr_schedule=cntk.learning_rate_schedule(learning_rate,cntk.UnitType.minibatch)
-    momentum_time_constant = cntk.momentum_as_time_constant_schedule(700)
-    learner=cntk.fsadagrad(net.parameters,lr=lr_schedule,momentum = momentum_time_constant,unit_gain = True)
+    momentum_time_constant = cntk.momentum_as_time_constant_schedule(1000,512)
+    learner=cntk.adadelta(net.parameters,lr_schedule,0.5,0.5)
     progres=cntk.logging.ProgressPrinter(0)
     trainer=cntk.Trainer(net,(loss,error),[learner],progress_writers=progres)
     input_map={
@@ -38,7 +39,7 @@ def train(streamf):
         
     }
     minibatch_size =  512
-    max_epochs = 100
+    max_epochs = 200
     epoch_size = 48985
     t = 0
     for epoch in range(max_epochs):
@@ -56,7 +57,7 @@ def test(streamf):
         label_var : streamf.streams.labels   
     }
     minibatch_size =  64
-    loss = cntk.cross_entropy_with_softmax(net,label_var)
+    loss = cntk.squared_error(net,label_var)
     progress_printer = cntk.logging.ProgressPrinter(tag='Evaluation', num_epochs=0)
     evaluator = cntk.eval.Evaluator(loss, progress_printer)
     while True:
@@ -67,10 +68,12 @@ def test(streamf):
     evaluator.summarize_test_progress()
 
 def teval(streamf,trainer):
-    model=trainer.model
+    z = load_model(".\\Model\\model.cmf")
     mb = streamf.next_minibatch(1000)
-    output = model.eval(mb[streamf.streams['features']])
-    for i in range(0,1000):
+    output = z.eval(mb[streamf.streams['features']].data.asarray())
+    print(mb[streamf.streams['features']].data.asarray())
+    print("===============================")
+    for i in range(len(output)):
         print(output[i])
 
 
