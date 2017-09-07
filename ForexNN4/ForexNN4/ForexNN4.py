4b7e4a41dabcb559281350316da086b28bbcea48
RandomKori <randomkori40@gmail.com>
2017-09-07 11:40:38 +0600

    v1.6b

--- ForexNN4/ForexNN4/ForexNN4.py
+++ ForexNN4/ForexNN4/ForexNN4.py
@@ -1,33 +1,35 @@
 from __future__ import print_function
 import numpy as np
 import cntk
+from cntk.ops.functions import load_model
 
 def LoadData(fn,is_training):
     n=".\\Data\\"+fn
-    datainp=cntk.io.StreamDef("features",15)
-    dataout=cntk.io.StreamDef("labels",1)
+    datainp=cntk.io.StreamDef("features",45)
+    dataout=cntk.io.StreamDef("labels",2)
     dataall=cntk.io.StreamDefs(features=datainp,labels=dataout)
     st=cntk.io.CTFDeserializer(n,dataall)
     mbs=cntk.io.MinibatchSource(st,randomize = is_training,max_sweeps = cntk.io.INFINITELY_REPEAT if is_training else 1)
     return mbs
 
 def nn(x):
-    with cntk.layers.default_options(initial_state = 0.1):
-        m = cntk.layers.Recurrence(cntk.layers.LSTM(15))(x)
-        m = cntk.sequence.last(m)
-        m = cntk.layers.Dropout(0.2)(m)
-        m = cntk.layers.Dense(1)(m)
+    m=cntk.layers.Stabilizer()(x)
+    for i in range(0,20):
+         m=cntk.layers.Recurrence(cntk.layers.RNNStep(45,activation=cntk.tanh))(m)
+    m=cntk.layers.Recurrence(cntk.layers.RNNStep(2,activation=cntk.tanh))(m)
     return m
 
+input_var = cntk.input_variable(45,np.float32, name = 'features',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+label_var=cntk.input_variable(2,np.float32, name = 'labels',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+
 def train(streamf):
-    input_var = cntk.input_variable(15,np.float32, name = 'features',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
-    label_var=cntk.input_variable(1,np.float32, name = 'labels')
+    global net
     net=nn(input_var)
     loss = cntk.squared_error(net,label_var)
     error=cntk.squared_error(net,label_var)
-    learning_rate=0.002
+    learning_rate=0.001
     lr_schedule=cntk.learning_rate_schedule(learning_rate,cntk.UnitType.minibatch)
-    momentum_time_constant = cntk.momentum_as_time_constant_schedule(5000 / -np.math.log(0.9))
+    momentum_time_constant = cntk.momentum_as_time_constant_schedule(140 / -np.math.log(0.9))
     learner=cntk.fsadagrad(net.parameters,lr=lr_schedule,momentum = momentum_time_constant,unit_gain = True)
     progres=cntk.logging.ProgressPrinter(0)
     trainer=cntk.Trainer(net,(loss,error),[learner],progress_writers=progres)
@@ -36,28 +38,40 @@ def train(streamf):
         label_var : streamf.streams.labels
         
     }
-    minibatch_size = 5000
-    num_samples_per_sweep = 5000
-    for i in range(0,num_samples_per_sweep):
-        dat1=streamf.next_minibatch(minibatch_size,input_map = input_map)
-        trainer.train_minibatch(dat1)
-        training_loss = trainer.previous_minibatch_loss_average
-        eval_error = trainer.previous_minibatch_evaluation_average
+    minibatch_size =  512
+    max_epochs = 200
+    epoch_size = 48985
+    t = 0
+    for epoch in range(max_epochs):
+        epoch_end = (epoch+1) * epoch_size
+        while t < epoch_end: 
+            dat1=streamf.next_minibatch(minibatch_size,input_map = input_map)
+            trainer.train_minibatch(dat1)
+            t += dat1[label_var].num_samples
+    trainer.summarize_training_progress()
     return trainer
 
-def test(streamf,trainer):
-    model=trainer.model
-    mb = streamf.next_minibatch(1000)
-    output = model.eval(mb[streamf.streams.features])
-    lsb=mb[streamf.streams.labels].data.asarray()
-    for i in range(0,1000):
-        print("[ {0} ]".format(output[i,0]))
+def test(streamf):
+    input_map={
+        input_var : streamf.streams.features,
+        label_var : streamf.streams.labels   
+    }
+    minibatch_size =  512
+    loss = cntk.squared_error(net,label_var)
+    progress_printer = cntk.logging.ProgressPrinter(tag='Evaluation', num_epochs=0)
+    evaluator = cntk.eval.Evaluator(loss, progress_printer)
+    while True:
+        dat1=streamf.next_minibatch(minibatch_size,input_map = input_map)
+        if not dat1:
+            break
+        evaluator.test_minibatch(dat1)
+    evaluator.summarize_test_progress()
 
 data=LoadData("train.txt",True)
 model1=train(data)
 md=model1.model
 md.save(".\\Model\\model.cmf")
 data1=LoadData("test.txt",False)
-test(data1,model1)
+test(data1)
 g=input("Нажмите любую клавишу")
 
