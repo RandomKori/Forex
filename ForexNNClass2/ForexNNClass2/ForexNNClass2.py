2f84bba1d950ed2184cab7f7845917e81c315bd1
RandomKori <randomkori40@gmail.com>
2017-09-11 19:17:03 +0600

    v1.7 ForexNN3 OK

--- ForexNNClass2/ForexNNClass2/ForexNNClass2.py
+++ ForexNNClass2/ForexNNClass2/ForexNNClass2.py
@@ -1,46 +1,53 @@
-﻿from __future__ import print_function
+from __future__ import print_function
 import numpy as np
 import cntk
 from cntk.ops.functions import load_model
 
 def LoadData(fn,is_training):
     n=".\\Data\\"+fn
-    datainps=cntk.io.StreamDef("spread",10)
-    datainph=cntk.io.StreamDef("high",10)
-    datainpl=cntk.io.StreamDef("low",10)
+    datainps=cntk.io.StreamDef("spread",20)
+    datainph=cntk.io.StreamDef("high",20)
+    datainpl=cntk.io.StreamDef("low",20)
+    datainpv=cntk.io.StreamDef("volume",20)
     dataout=cntk.io.StreamDef("labels",3,is_sparse=True)
-    dataall=cntk.io.StreamDefs(spread=datainps,high=datainph,low=datainpl,labels=dataout)
+    dataall=cntk.io.StreamDefs(spread=datainps,high=datainph,low=datainpl,volume=datainpv,labels=dataout)
     st=cntk.io.CTFDeserializer(n,dataall)
     mbs=cntk.io.MinibatchSource(st,randomize = is_training,max_sweeps = cntk.io.INFINITELY_REPEAT if is_training else 1)
     return mbs
 
-def nn(s,h,l):
-    m=cntk.layers.Recurrence(cntk.layers.RNNStep(50,activation=cntk.sigmoid,init_bias=0.1))(s)
+def nn(s,h,l,v):
+    m=cntk.layers.Stabilizer()(s)
     for i in range(0,5):
-        m=cntk.layers.Recurrence(cntk.layers.RNNStep(50,activation=cntk.sigmoid,init_bias=0.1))(m)
+        m=cntk.layers.Recurrence(cntk.layers.LSTM(50,activation=cntk.sigmoid,init_bias=0.1,enable_self_stabilization=True))(m)
     m=cntk.splice(m,h)
-    m=cntk.layers.Recurrence(cntk.layers.RNNStep(50,activation=cntk.sigmoid,init_bias=0.1))(m)
+    m=cntk.layers.Stabilizer()(m)
     for i in range(0,5):
-        m1=cntk.layers.Recurrence(cntk.layers.RNNStep(50,activation=cntk.sigmoid,init_bias=0.1))(m)
+        m1=cntk.layers.Recurrence(cntk.layers.LSTM(50,activation=cntk.sigmoid,init_bias=0.1,enable_self_stabilization=True))(m)
     m=cntk.splice(m,l)
-    m=cntk.layers.Recurrence(cntk.layers.RNNStep(50,activation=cntk.sigmoid,init_bias=0.1))(m)
+    m=cntk.layers.Stabilizer()(m)
     for i in range(0,5):
-        m2=cntk.layers.Recurrence(cntk.layers.RNNStep(50,activation=cntk.sigmoid,init_bias=0.1))(m)
-    m=cntk.layers.Recurrence(cntk.layers.RNNStep(3,activation=cntk.softmax))(m)
+        m2=cntk.layers.Recurrence(cntk.layers.LSTM(50,activation=cntk.sigmoid,init_bias=0.1,enable_self_stabilization=True))(m)
+    m=cntk.splice(m,v)
+    m=cntk.layers.Stabilizer()(m)
+    for i in range(0,5):
+        m=cntk.layers.Recurrence(cntk.layers.LSTM(50,activation=cntk.sigmoid,init_bias=0.1,enable_self_stabilization=True))(m)
+    m=cntk.sequence.last(m)
+    m=cntk.layers.Dense(3,activation=cntk.softmax)(m)
     return m
 
-input_s = cntk.input_variable(10,np.float32, name = 'spread',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
-input_h = cntk.input_variable(10,np.float32, name = 'high',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
-input_l = cntk.input_variable(10,np.float32, name = 'low',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
-label_var=cntk.input_variable(3,np.float32, name = 'labels',is_sparse=True,dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+input_s = cntk.input_variable(20,np.float32, name = 'spread',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+input_h = cntk.input_variable(20,np.float32, name = 'high',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+input_l = cntk.input_variable(20,np.float32, name = 'low',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+input_v = cntk.input_variable(20,np.float32, name = 'volume',dynamic_axes=cntk.axis.Axis.default_input_variable_dynamic_axes())
+label_var=cntk.input_variable(3,np.float32, name = 'labels',is_sparse=True)
 
 
 def train(streamf):
     global net
-    minibatch_size =  1024
+    minibatch_size =  512
     max_epochs = 2000
     epoch_size = 48985
-    net=nn(input_s,input_h,input_l)
+    net=nn(input_s,input_h,input_l,input_v)
     loss = cntk.losses.cross_entropy_with_softmax(net,label_var)
     error=cntk.classification_error(net,label_var)
     lr_per_sample = [3e-4]*4+[1.5e-4]
@@ -54,6 +61,7 @@ def train(streamf):
         input_s : streamf.streams.spread,
         input_h : streamf.streams.high,
         input_l : streamf.streams.low,
+        input_v : streamf.streams.volume,
         label_var : streamf.streams.labels
         
     }
@@ -72,6 +80,7 @@ def test(streamf):
         input_s : streamf.streams.spread,
         input_h : streamf.streams.high,
         input_l : streamf.streams.low,
+        input_v : streamf.streams.volume,
         label_var : streamf.streams.labels   
     }
     minibatch_size =  32
@@ -91,6 +100,7 @@ def feval(streamf):
         z.arguments[0] : streamf.streams.spread, 
         z.arguments[1] : streamf.streams.high,
         z.arguments[2] : streamf.streams.low,
+        z.arguments[3] : streamf.streams.volume
     }
     dat1=streamf.next_minibatch(1000,input_map = input_map)
     output=z.eval(dat1)
@@ -108,3 +118,4 @@ test(data1)
 data2=LoadData("test.txt",False)
 feval(data2)
 g=input("Нажмите любую клавишу")
+
